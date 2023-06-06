f57d215b648242e8a315f2a30758f18d0ffc78a0
manuel_humanes_kairosds <manuel.humanes@kairosds.com>
2023-06-06 18:44:38 +0200

    Added conditional destination for uploaded image

--- src/routes/task.route.js
+++ src/routes/task.route.js
@@ -1,16 +1,29 @@
 const router = require('express').Router();
 const taskController = require('./controllers/task.controller');
 const multer = require('multer');
+const fs = require('fs-extra');
+
 const upload = multer({
     storage: multer.diskStorage({
-        destination: './uploads/',
+        destination: function (req, file, cb) {
+            if(file.fieldname === 'image') {
+                const path = './output/original';
+                fs.mkdirsSync(path);
+                cb(null, path);
+            }
+            else {
+                const path = `./output/${file.fieldname}`;
+                fs.mkdirSync(path)
+                cb(null, path);
+            }
+        },
         limits: { fileSize: 10 * 1024 * 1024 },
         filename: function (req, file, cb) {
             cb(null, `${Date.now()}-${file.originalname}`);
         }
     })
 });
-//Raiz
+
 router.post('/', upload.single('image'), taskController.createTask)
 router.get('/:id', taskController.getTaskDetail)
  
