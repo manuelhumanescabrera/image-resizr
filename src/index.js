46fc6ae12b5d25928400299fb27d7d32b63d4c85
manuel_humanes_kairosds <manuel.humanes@kairosds.com>
2023-06-06 22:01:02 +0200

    Added mongodb and mongoose connection

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const app = express();
 const morgan=require('morgan');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
+mongoose.connect(process.env.MONGODB_URL).catch((ex) => console.error('Error connecting to database...', { ex }));
+
+mongoose.connection.on('error', (error) => {
+    console.log(error)
+})
+
+mongoose.connection.once('connected', () => {
+    console.log('Database Connected');
+})
 //Configuraciones
 app.set('port', process.env.PORT || 3000);
 app.set('json spaces', 2)
