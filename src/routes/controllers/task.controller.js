7c69808a86cecc08d58dcd57bdd4cbdbdc6d924d
manuel_humanes_kairosds <manuel.humanes@kairosds.com>
2023-06-06 22:03:32 +0200

    Added image's task database creation

--- src/routes/controllers/task.controller.js
+++ src/routes/controllers/task.controller.js
@@ -1,7 +1,16 @@
+const Task = require('../../model/task.model');
+const TaskStatus = require('../../constants/task.constants');
+
 const createTask = async(req, res, next) => {
-    res.json({
-        file: {...req.file}
-    })
+    const task = new Task({status: TaskStatus.PROCCESSING, originalImage: req.file.path})
+    try {
+        await task.save();
+        res.status(200).json(task.doc)
+    }
+    catch(ex) {
+        console.error('Error creating task in database', { ex });
+        res.status(500).json({ error: ex.message })
+    }
     next();
 }
 const getTaskDetail = async(req, res, next) => {
