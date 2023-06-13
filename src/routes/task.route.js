eae31b0051b667b1943d11635d54ba3e9e69590e
manuel_humanes_kairosds <manuel.humanes@kairosds.com>
2023-06-13 18:40:48 +0200

    Changed destination files folder

--- src/routes/task.route.js
+++ src/routes/task.route.js
@@ -1,22 +1,10 @@
 const router = require('express').Router();
 const taskController = require('./controllers/task.controller');
 const multer = require('multer');
-const fs = require('fs-extra');
 
 const upload = multer({
     storage: multer.diskStorage({
-        destination: function (req, file, cb) {
-            if(file.fieldname === 'image') {
-                const path = './output/original';
-                fs.mkdirsSync(path);
-                cb(null, path);
-            }
-            else {
-                const path = `./output/${file.fieldname}`;
-                fs.mkdirSync(path)
-                cb(null, path);
-            }
-        },
+        destination: '/tmp',
         limits: { fileSize: 10 * 1024 * 1024 },
         filename: function (req, file, cb) {
             cb(null, `${Date.now()}-${file.originalname}`);
@@ -25,6 +13,5 @@ const upload = multer({
 });
 
 router.post('/', upload.single('image'), taskController.createTask)
-router.get('/:id', taskController.getTaskDetail)
- 
+
 module.exports = router;
